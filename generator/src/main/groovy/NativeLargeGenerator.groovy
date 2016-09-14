0cf4f6feebb298916335b14d49ba1e15b39a5095
Lari Hotari <lari@hotari.net>
2016-09-14 11:39:41 +0300

    Implement parser

--- generator/src/main/groovy/NativeLargeGenerator.groovy
+++ generator/src/main/groovy/NativeLargeGenerator.groovy
@@ -1,4 +1,7 @@
+import groovy.transform.Canonical
 import groovy.transform.TupleConstructor
+
+import java.util.regex.Pattern
 
 @TupleConstructor
 class NativeLargeGenerator {
@@ -7,14 +10,65 @@ class NativeLargeGenerator {
 
     void generate() {
         outputDir.mkdirs()
-
-        reportFile.eachLine { String line ->
-            println line
-        }
+        List<Project> projects = ReportParser.parse(reportFile)
+        println projects
     }
 
     static void main(String[] args) {
         def generator = new NativeLargeGenerator(new File('report.txt'), new File('/tmp/native-large'))
         generator.generate()
     }
+}
+
+
+class ReportParser {
+    static Pattern MODULE_PATTERN = ~/# (\S+) with (.*)$/
+    static Pattern WHITESPACE_PATTERN = ~/\s+/
+
+    static List<Project> parse(File reportFile) {
+        List<Project> projects = []
+        Project currentProject
+        reportFile.eachLine { String line ->
+            line = line.trim()
+            if (line) {
+                if (line.startsWith('project ')) {
+                    if (currentProject != null) {
+                        projects << currentProject
+                    }
+                    currentProject = new Project()
+                    currentProject.name = line.substring('project '.length()).trim()
+                } else {
+                    def m = (line =~ MODULE_PATTERN)
+                    if (m.matches()) {
+                        def moduleType = m.group(1)
+                        Module module = new Module(type: moduleType)
+                        currentProject.modules << module
+                        for (List attributePair : (WHITESPACE_PATTERN.split(m.group(2)) as List).collate(2)) {
+                            int value = attributePair.get(0) as int
+                            String field = attributePair.get(1)
+                            module.setProperty(field, value)
+                        }
+                    }
+                }
+            }
+        }
+        if (currentProject != null) {
+            projects << currentProject
+        }
+    }
+}
+
+@Canonical
+class Project {
+    String name
+    List<Module> modules = []
+}
+
+@Canonical
+class Module {
+    String type
+    int sources
+    int headers
+    int includePaths
+    int dependencies
 }
