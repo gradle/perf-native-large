5bad79b07e960169a3b46e9079c05b506996caef
Pepper Lebeck-Jobe <pepper@gradle.com>
2016-09-14 20:44:49 +0200

    Simplify the GradleComponentType enum
    We don't need the from() method or to track the boolean value in the
    enum.

--- generator/src/main/groovy/GradleComponentType.groovy
+++ generator/src/main/groovy/GradleComponentType.groovy
@@ -2,29 +2,14 @@ import groovy.transform.CompileStatic
 
 @CompileStatic
 enum GradleComponentType {
-    GOOGLE_TEST_TEST_SUITE_SPEC("GoogleTestTestSuiteSpec", true),
-    NATIVE_EXECUTABLE_SPEC("NativeExecutableSpec", true),
-    NATIVE_LIBRARY_SPEC("NativeLibrarySpec", true),
-    NATIVE_LIBRARY_SPEC_UNSHARED("NativeLibrarySpec", false)
+    GOOGLE_TEST_TEST_SUITE_SPEC("GoogleTestTestSuiteSpec"),
+    NATIVE_EXECUTABLE_SPEC("NativeExecutableSpec"),
+    NATIVE_LIBRARY_SPEC("NativeLibrarySpec"),
+    PREBUILT_LIBRARY("PrebuiltLibrary")
 
     final String name
-    final boolean sharedLibraryEnabled
-    static final Map<String, GradleComponentType> reverseMap
 
-    static {
-        reverseMap = [:] as TreeMap
-        values().each { GradleComponentType type ->
-            reverseMap.put(type.name, type)
-        }
-    }
-
-    private GradleComponentType(String name, boolean sharedLibraryEnabled) {
+    private GradleComponentType(String name) {
         this.name = name
-        this.sharedLibraryEnabled = sharedLibraryEnabled
-    }
-
-
-    static GradleComponentType from(String type) {
-        reverseMap[type]
     }
 }
